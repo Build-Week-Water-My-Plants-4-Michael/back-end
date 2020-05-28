83294f845a572a1436276967c92242ae6ff94bd4
candaceyw <candaceyw@gmail.com>
2020-05-28 14:05:25 -0400

    remove cookie

--- routes/users/auth-middleware.js
+++ routes/users/auth-middleware.js
@@ -24,7 +24,7 @@ function authenticate() {
     //     }
     //   };
 
-    const token = req.headers.token;
+    const token = req.body.token;
 
     // console.log(token)
 
--- routes/users/users-router.js
+++ routes/users/users-router.js
@@ -24,80 +24,44 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
-// router.post('/login', validateUsername, async (req, res, next) => {
-// 	const authError = {
-// 		message: 'Invalid Credentials',
-// 	};
+router.post('/login', validateUsername, async (req, res, next) => {
+  const authError = {
+    message: 'Invalid Credentials',
+  };
 
-// 	try {
-// 		const user = await Users.findUserBy({
-// 			username: req.body.username,
-// 		}).first();
-// 		if (!user) {
-// 			return res.status(401).json(authError);
-// 		}
-
-// 		const passwordValid = await bcrypt.compare(
-// 			req.body.password,
-// 			user.password
-// 		);
-// 		if (!passwordValid) {
-// 			return res.status(401).json(authError);
-// 		}
-
-// 		const tokenPayload = {
-// 			userId: user.id,
-// 			username: user.username,
-// 			phoneNumber: user.phoneNumber,
-// 		};
-
-// 		const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
-// 		res.cookie('token', token);
-
-// 		res.json({
-// 			message: `Welcome, ${user.username}!`,
-// 			token: token,
-// 		});
-// 	} catch (err) {
-// 		next(err);
-// 	}
-// });
-
-router.post('/login', async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-
-    const user = await Users.findBy({ username }).first();
-
-    const passwordValid = await bcrypt.compare(password, user.password);
-
-    if (user && passwordValid) {
-      const token = generateToken(user);
-      const userInfo = await Users.findById(user.id).first();
-      res.status(200).json({
-        token: token,
-        user: userInfo,
-      });
-    } else {
-      res.status(401).json({ message: 'Authentication Failed' });
+    const user = await Users.findUserBy({
+      username: req.body.username,
+    }).first();
+    if (!user) {
+      return res.status(401).json(authError);
     }
+
+    const passwordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passwordValid) {
+      return res.status(401).json(authError);
+    }
+
+    const tokenPayload = {
+      userId: user.id,
+      username: user.username,
+      phoneNumber: user.phoneNumber,
+    };
+
+    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
+    res.body('token', token);
+
+    res.json({
+      message: `Welcome, ${user.username}!`,
+      token: token,
+    });
   } catch (err) {
-    res.status(401).json({ message: 'Authentication Failed' });
+    next(err);
   }
 });
-
-function generateToken(user) {
-  const payload = {
-    id: user.id,
-    username: user.username,
-  };
-
-  const options = {
-    expiresIn: '1h',
-  };
-
-  return jwt.sign(payload, process.env.JWT_SECRET, options);
-}
 
 async function validateUsername(req, res, next) {
   try {
