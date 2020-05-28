8585525f643d262a885c5cf9d990c330b9ef5429
Candace Wilson <candaceyw@gmail.com>
2020-05-27 22:58:35 -0400

    token auth

--- routes/users/auth-middleware.js
+++ routes/users/auth-middleware.js
@@ -6,8 +6,9 @@ function authenticate() {
 			message: 'You shall not pass!',
 		};
 		try {
-			const token = req.headers.token;
-			console.log(req.headers.token);
+			// const token = req.cookies.token;
+			const token = req.headers.authorization;
+			console.log(req.headers.authorization);
 			if (!token) {
 				return res.status(401).json(authErr);
 			}
