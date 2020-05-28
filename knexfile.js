7103375e2606624c048c753d8cd06a1c82458c84
candaceyw <candaceyw@gmail.com>
2020-05-28 13:41:59 -0400

    auth change

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,29 @@
 module.exports = {
-	// development: {
-	// 	client: 'pg',
-	// 	connection: 'postgres://candacewilson:postgres@localhost/waterPlants',
-	// 	migrations: {
-	// 		directory: './database/migrations',
-	// 		tableName: 'waterPlants',
-	// 	},
-	// 	seeds: { directory: './database/seeds' },
-	// },
-	production: {
-		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		migrations: {
-			directory: './database/migrations',
-			tableName: 'waterPlants',
-		},
-		seeds: { directory: './database/seeds' },
-	},
-	test: {
-		client: 'pg',
-		connection: 'postgres://candacewilson:postgres@localhost/test-waterPlants',
-		migrations: {
-			directory: './database/migrations',
-			tableName: 'waterPlants',
-		},
-		seeds: { directory: './database/seeds' },
-	},
+  development: {
+    client: 'pg',
+    connection: 'postgres://candacewilson:postgres@localhost/waterPlants',
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'waterPlants',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'waterPlants',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://candacewilson:postgres@localhost/test-waterPlants',
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'waterPlants',
+    },
+    seeds: { directory: './database/seeds' },
+  },
 };
--- routes/users/auth-middleware.js
+++ routes/users/auth-middleware.js
@@ -2,27 +2,40 @@ const jwt = require('jsonwebtoken');
 
 function authenticate() {
   return async (req, res, next) => {
-    const authErr = {
-      message: 'You shall not pass!',
-    };
+    //     const authErr = {
+    //       message: 'You shall not pass!',
+    //     };
+    //     try {
+    //       const token = req.body.token;
+    //       // const token = req.headers.authorization;
+    //       // console.log(req.headers.authorization);
+    //       if (!token) {
+    //         return res.status(401).json(authErr);
+    //       }
+    //       jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
+    //         if (err) {
+    //           return res.status(401).json(authErr);
+    //         }
+    //         req.token = decodedPayload;
+    //         next();
+    //       });
+    //     } catch (err) {
+    //       next(err);
+    //     }
+    //   };
+
+    const token = req.body.token;
+
+    // console.log(token)
+
     try {
-      const token = req.body.token;
-      // const token = req.headers.authorization;
-      // console.log(req.headers.authorization);
-      if (!token) {
-        return res.status(401).json(authErr);
-      }
-      jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
-        if (err) {
-          return res.status(401).json(authErr);
-        }
-        req.token = decodedPayload;
-        next();
-      });
+      const decoded = jwt.verify(token, secrets.jwtSecret);
+      req.decoded = decoded;
+      // console.log(decoded);
+      next();
     } catch (err) {
-      next(err);
+      res.status(401).json({ message: 'You shall not pass!' });
     }
   };
 }
-
 module.exports = authenticate;
