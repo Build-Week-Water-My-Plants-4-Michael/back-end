2ee0138b8a55065a520409cd18b514c1f1529992
candaceyw <candaceyw@gmail.com>
2020-05-28 13:58:29 -0400

    oops

--- routes/users/users-model.js
+++ routes/users/users-model.js
@@ -3,32 +3,37 @@ const db = require('../../database/knex');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-	findUser,
-	userById,
-	findUserBy,
-	addUser,
+  findUser,
+  findBy,
+  userById,
+  findUserBy,
+  addUser,
 };
 
 function findUser() {
-	return db('users').select('id', 'username', 'phoneNumber');
+  return db('users').select('id', 'username', 'phoneNumber');
+}
+
+function findBy(filter) {
+  return db('users').where(filter).first('id', 'username', 'password');
 }
 
 function userById(id) {
-	return db('users').where('id', id).first();
+  return db('users').where('id', id).first();
 }
 
 function findUserBy(filter) {
-	return db('users')
-		.select('id', 'username', 'phoneNumber', 'password')
-		.where(filter);
+  return db('users')
+    .select('id', 'username', 'phoneNumber', 'password')
+    .where(filter);
 }
 
 async function addUser(user) {
-	// hash the password with a time complexity of 14
-	user.password = await bcrypt.hash(user.password, 14);
+  // hash the password with a time complexity of 14
+  user.password = await bcrypt.hash(user.password, 14);
 
-	const [id] = await db('users')
-		.insert(user)
-		.returning('id', 'username', 'password', 'phoneNumber');
-	return userById(id);
+  const [id] = await db('users')
+    .insert(user)
+    .returning('id', 'username', 'password', 'phoneNumber');
+  return userById(id);
 }
